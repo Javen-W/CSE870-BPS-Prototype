0d404bf7716317baaaf8248f18f1cbb5ab2a45df
Javen <javen.william@gmail.com>
2025-03-18 15:14:52 -0400

    impl state_machine

--- scenes/Car01.cs
+++ scenes/Car01.cs
@@ -1,15 +1,18 @@
 using Godot;
 using System;
 
-public partial class Car01 : VehicleBody3D
+namespace CSE849BPSPrototype
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	public partial class Car01 : VehicleBody3D
 	{
-	}
+		// Called when the node enters the scene tree for the first time.
+		public override void _Ready()
+		{
+		}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+		// Called every frame. 'delta' is the elapsed time since the previous frame.
+		public override void _Process(double delta)
+		{
+		}
 	}
 }
